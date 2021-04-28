2ad0d10402a041cf832f522e94b1a3acb6155857
skarry <60601122@mail.ru>
2021-04-29 01:48:26 +0300

    add new view controller

--- FavoritePlaces/MainController.swift
+++ FavoritePlaces/MainController.swift
@@ -53,5 +53,7 @@ class MainController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func segueExit(_ segue: UIStoryboardSegue) {}
 
 }
