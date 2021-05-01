64381897026e16d59b2391e31bb3afab78be2f6c
skarry <60601122@mail.ru>
2021-05-01 17:35:29 +0300

    added item deletion

--- FavoritePlaces/MainController.swift
+++ FavoritePlaces/MainController.swift
@@ -45,7 +45,14 @@ class MainController: UITableViewController {
         return cell
     }
     
+    // MARK: - Table view delegate
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            StorageManager.deleteObject(places[indexPath.row])
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 
     /*
     // MARK: - Navigation
